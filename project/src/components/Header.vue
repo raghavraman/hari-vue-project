f06cc5ca02c5168c11eb73410e0b03725eee01de
Raghav Raman <rghv.raman@gmail.com>
2021-01-14 01:29:28 -0500

    added commit

---
+++ project/src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <h1>{{message}}</h1>
+      <p>Page no {{number}}</p>
+      <button @click=callGoogle()>click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        message: {
+            type: String,
+            default: "Hello my name is header"
+        },
+        number: {
+            type: Number,
+            required: true
+        }
+    },
+    methods: {
+        callGoogle(){
+            // alert("hey you called this method");
+            window.location ="https://google.com";
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
